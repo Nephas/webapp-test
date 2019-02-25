78168621b56e73d761afe9a72f56c4acf1c0d0cf
Marco Fink <marco.fink@zoho.eu>
2019-02-25 21:27:06 +0100

    Revert "some renamings"
    This reverts commit 18fdc8f

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject webapp-test "1.0.0-SNAPSHOT"
-  :description "Clojure web app"
+  :description "Demo Clojure web app"
   :url "http://clojure-getting-started.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
                  [compojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
-  :uberjar-name "server-standalone.jar"
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "webapp-test-standalone.jar"
   :profiles {:production {:env {:production true}}})
--- src/webapp_test/web.clj
+++ src/webapp_test/web.clj
@@ -1,9 +1,10 @@
-(ns server.core
+(ns webapp-test.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
             [clojure.java.io :as io]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
 
 (defroutes app
            (GET "/" [] (slurp (io/resource "index.html")))
@@ -11,4 +12,5 @@
            (route/not-found (slurp (io/resource "404.html"))))
 
 (defn -main [& [port]]
-  (jetty/run-jetty (site #'app) {:port 5000}))
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
